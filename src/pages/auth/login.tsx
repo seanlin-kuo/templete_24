02f4e3cb9519ea6cd58ea9d563f3e50bdc8ab006
youshenlin <mikey8316@gmail.com>
2021-06-22 17:25:19 +0800

    fix

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,26 +1,57 @@
 import { Button } from '@paljs/ui/Button';
 import { InputGroup } from '@paljs/ui/Input';
 import { Checkbox } from '@paljs/ui/Checkbox';
-import React from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
-
+import { useRouter } from 'next/router';
 import Auth, { Group } from 'components/Auth';
 import Socials from 'components/Auth/Socials';
 import Layout from 'Layouts';
+import axios from 'axios';
 
 export default function Login() {
+  const router = useRouter();
+  const [postData, setPostData] = useState({});
   const onCheckbox = () => {
     // v will be true or false
+  };
+  const login = () => {
+    axios
+      .post(
+        'https://sean-nimda-42.dev.kuobrothers.com/login',
+        {
+          account: postData.email,
+          pass: postData.password,
+        },
+        {
+          withCredentials: true,
+        },
+      )
+      .then((res) => {
+        if (res.data.errCode != '0000') {
+          alert('登入失敗');
+          return;
+        }
+        window.location.replace('/extra-components/accordion');
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  const handleChange = (event) => {
+    let post = { ...postData };
+    post[event.target.name] = event.target.value;
+    setPostData(post);
   };
   return (
     <Layout title="Login">
       <Auth title="Login" subTitle="Hello! Login with your email">
         <form>
           <InputGroup fullWidth>
-            <input type="email" placeholder="Email Address" />
+            <input name="email" type="email" placeholder="Email Address" onChange={handleChange} />
           </InputGroup>
           <InputGroup fullWidth>
-            <input type="password" placeholder="Password" />
+            <input name="password" type="password" placeholder="Password" onChange={handleChange} />
           </InputGroup>
           <Group>
             <Checkbox checked onChange={onCheckbox}>
@@ -30,7 +61,7 @@ export default function Login() {
               <a>Forgot Password?</a>
             </Link>
           </Group>
-          <Button status="Success" type="button" shape="SemiRound" fullWidth>
+          <Button status="Success" type="button" onClick={login} shape="SemiRound" fullWidth>
             Login
           </Button>
         </form>
